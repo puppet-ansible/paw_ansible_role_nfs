0d595f968ff8cdcb105c02b1c38875aeda0dde6b
Chris Allen <chris.j.allen1979@gmail.com>
2026-02-04 12:49:40 +0000

    Automated conversion from geerlingguy.nfs Ansible role

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ NFS installation for Linux.
 
 ## Conversion Details
 
-- **Converted on**: 2025-11-18
+- **Converted on**: 2026-02-04
 - **Original Author**: geerlingguy
 - **License**: license (BSD, MIT)
 
--- manifests/init.pp
+++ manifests/init.pp
@@ -5,6 +5,7 @@
 # @param nfs_exports
 # @param nfs_rpcbind_state
 # @param nfs_rpcbind_enabled
+# @param par_vardir Base directory for Puppet agent cache (uses lookup('paw::par_vardir') for common config)
 # @param par_tags An array of Ansible tags to execute (optional)
 # @param par_skip_tags An array of Ansible tags to skip (optional)
 # @param par_start_at_task The name of the task to start execution at (optional)
@@ -21,6 +22,7 @@ class paw_ansible_role_nfs (
   Array $nfs_exports = [],
   String $nfs_rpcbind_state = 'started',
   Boolean $nfs_rpcbind_enabled = true,
+  Optional[Stdlib::Absolutepath] $par_vardir = undef,
   Optional[Array[String]] $par_tags = undef,
   Optional[Array[String]] $par_skip_tags = undef,
   Optional[String] $par_start_at_task = undef,
@@ -34,34 +36,33 @@ class paw_ansible_role_nfs (
   Optional[Boolean] $par_exclusive = undef
 ) {
 # Execute the Ansible role using PAR (Puppet Ansible Runner)
-  $vardir = $facts['puppet_vardir'] ? {
-    undef   => $settings::vardir ? {
-      undef   => '/opt/puppetlabs/puppet/cache',
-      default => $settings::vardir,
-    },
-    default => $facts['puppet_vardir'],
-  }
-  $playbook_path = "${vardir}/lib/puppet_x/ansible_modules/ansible_role_nfs/playbook.yml"
+# Playbook synced via pluginsync to agent's cache directory
+# Check for common paw::par_vardir setting, then module-specific, then default
+$_par_vardir = $par_vardir ? {
+  undef   => lookup('paw::par_vardir', Stdlib::Absolutepath, 'first', '/opt/puppetlabs/puppet/cache'),
+  default => $par_vardir,
+}
+$playbook_path = "${_par_vardir}/lib/puppet_x/ansible_modules/ansible_role_nfs/playbook.yml"
 
-  par { 'paw_ansible_role_nfs-main':
-    ensure        => present,
-    playbook      => $playbook_path,
-    playbook_vars => {
-      'export'              => $export,
-      'nfs_exports'         => $nfs_exports,
-      'nfs_rpcbind_state'   => $nfs_rpcbind_state,
-      'nfs_rpcbind_enabled' => $nfs_rpcbind_enabled,
-    },
-    tags          => $par_tags,
-    skip_tags     => $par_skip_tags,
-    start_at_task => $par_start_at_task,
-    limit         => $par_limit,
-    verbose       => $par_verbose,
-    check_mode    => $par_check_mode,
-    timeout       => $par_timeout,
-    user          => $par_user,
-    env_vars      => $par_env_vars,
-    logoutput     => $par_logoutput,
-    exclusive     => $par_exclusive,
-  }
+par { 'paw_ansible_role_nfs-main':
+  ensure        => present,
+  playbook      => $playbook_path,
+  playbook_vars => {
+        'export' => $export,
+        'nfs_exports' => $nfs_exports,
+        'nfs_rpcbind_state' => $nfs_rpcbind_state,
+        'nfs_rpcbind_enabled' => $nfs_rpcbind_enabled
+              },
+  tags          => $par_tags,
+  skip_tags     => $par_skip_tags,
+  start_at_task => $par_start_at_task,
+  limit         => $par_limit,
+  verbose       => $par_verbose,
+  check_mode    => $par_check_mode,
+  timeout       => $par_timeout,
+  user          => $par_user,
+  env_vars      => $par_env_vars,
+  logoutput     => $par_logoutput,
+  exclusive     => $par_exclusive,
+}
 }
